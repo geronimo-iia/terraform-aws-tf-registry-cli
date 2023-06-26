68705bfc13feff94bf5deab3910f4256d3ddf68d
Jerome Guibert <jguibert@gmail.com>
2023-06-27 00:18:04 +0200

    doc: add unpublish_module

--- aws_terraform_registry/__init__.py
+++ aws_terraform_registry/__init__.py
@@ -1,5 +1,5 @@
 from .cli import build_parser
-from .common import TerraformModuleIdentifier, generate_terraformrc, generate_token, publish_module, release_module
+from .common import TerraformModuleIdentifier, generate_terraformrc, generate_token, publish_module, release_module, unpublish_module
 from .config import ApplicationConfig
 
 # for auto documentation
@@ -10,5 +10,6 @@ __all__ = [
     'generate_terraformrc',
     'generate_token',
     'publish_module',
+    'unpublish_module',
     'release_module',
 ]
